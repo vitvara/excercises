49c62ae9fae60372142549abc88ad98f8b1bf4cc
doawemr <doaw_12@hotmail.com>
2026-09-05 15:53:28 +0700

    deanGreet (#12)
    Co-authored-by: doawemr DoawEMR <doawemr@doaw_12@hotmail.com>

---
+++ src/features/deanGreet.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const { register } = require('../registry');
+
+register(
+  'deanGreet',
+  {
+    description: 'Says hello to whoever is passed in.',
+    author: 'starter-kit',
+    addedIn: 'v0.1.0',
+  },
+  (who = 'world') => `Hello, ${who}!
+);
