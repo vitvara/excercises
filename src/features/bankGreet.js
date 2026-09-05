8dd4630615be6491f051175b99625bbc7cb83d7a
tanawit-emr <tanawit.khunha@hotmail.com>
2026-09-05 15:03:07 +0700

    change addedIn v0.1.1

--- src/features/bankGreet.js
+++ src/features/bankGreet.js
@@ -7,7 +7,7 @@ register(
   {
     description: 'Says hello to whoever is passed in.',
     author: 'starter-kit',
-    addedIn: 'v0.1.0',
+    addedIn: 'v0.1.1',
   },
   (who = 'world') => `Hello, ${who}!`
 );
